61b1ce3d47fc943a01df72299ebd4d75f1f68061
Greg Reeve <greg_reeve@byu.edu>
2017-03-10 21:57:08 -0700

    Add chp1 exercises

---
+++ fluent_python/chp1.py
@@ -0,0 +1,52 @@
+import collections
+from random import choice
+from math import hypot
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    # suits = 'spades diamonds clubs hearts'.split()
+    suits = '♠️ ♦️ ♣️ ♥️'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits 
+                                        for rank in self.ranks]
+    
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position): 
+        return self._cards[position]
+
+
+# do stuff with the FrenchDeck class
+
+print(Card('7', '♦️'))
+
+deck = FrenchDeck()
+print(len(deck))
+print([choice(deck) for x in range(3)])
+        
+
+class Vector:
+    def __init__(self, x=0, y=0): 
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector({}, {})'.format(self.x, self.y)
+        
+    def __abs__(self):
+        return hypot(self.x, self.y)
+        
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def __add__(self, other): 
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
